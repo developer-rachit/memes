6e82eff806c1445cb8faf7819d35b77cfd51fa69
Rachit3988 <rachit.cs3988@gmail.com>
2022-07-29 03:48:22 +0530

    title changed

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Memes :}',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Memes :}'),
     );
   }
 }
